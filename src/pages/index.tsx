656f7b5cf9317f323bb3a29a10b4ccfd2242a20a
Jakub Antczak <jakubantczak.info@gmail.com>
2024-09-02 15:46:36 +0200

    MPRO-001: Created example UI for the demo app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import {useEffect, useState} from 'react'
-import styles from './page.module.css'
 import {ExternalProvider, Web3Provider} from '@ethersproject/providers'
 import {useRouter} from 'next/router'
 import {CheckWalletResponse, CreateOrLoginResponse, User} from '@/config/types'
@@ -157,11 +156,14 @@ const handleExampleSignature = async (
 export default function Home() {
 	const [account, setAccount] = useState('')
 	const [chainId, setChainId] = useState(0)
-	const [provider, setProvider] = useState<any>()
-	const [currentUserID, setCurrentUserID] = useState('')
+	const [provider, setProvider] = useState<Web3Provider>()
+	const [displayRulesCheckbox, setDisplayRulesCheckbox] = useState(true)
+	const [rulesChecked, setRulesChecked] = useState(true)
+
+	const {push} = useRouter()
 
 	useEffect(() => {
-		const handleWeb3Login = async () => {
+		const handleWeb3 = async () => {
 			try {
 				const provider = new Web3Provider(window.ethereum as ExternalProvider)
 				// To prevent TS error add window.ethereum to window object in global.d.ts
@@ -170,20 +172,30 @@ export default function Home() {
 					const signer = await provider.getSigner()
 					const account = await signer.getAddress()
 					const network = await provider.getNetwork()
+					const localLowerCaseAccount = account.toLowerCase()
 					setChainId(network.chainId)
-					setAccount(account.toLowerCase())
+					setAccount(localLowerCaseAccount)
 					window.ethereum.on('accountsChanged', async (accounts: string[]) => {
 						setAccount(accounts[0].toLowerCase())
 					})
 					window.ethereum.on('chainChanged', async (chainId: string) => {
 						setChainId(parseInt(chainId, 16))
 					})
+					const checkUserExistanceResponse = await checkIfUserExists(
+						localLowerCaseAccount,
+					)
+					// When user does not exists in metapro we can handle logic for example to check rules and create account
+					if (checkUserExistanceResponse?.rulesCheckbox) {
+						setDisplayRulesCheckbox(checkUserExistanceResponse?.rulesCheckbox)
+						setRulesChecked(false)
+						return
+					}
 				}
 			} catch (error) {
 				console.error(error)
 			}
 		}
-		handleWeb3Login()
+		handleWeb3()
 
 		return () => {
 			if (provider) {
@@ -195,101 +207,52 @@ export default function Home() {
 
 	const handleMetaproLogin = async () => {
 		try {
-			const provider = new Web3Provider(window.ethereum as ExternalProvider)
-			// Create get request to https://api.metaproprotocol.com/users-service/auth/signature/hash to get hash
-			// Headers should include x-account-wallet with the account address
-			const hashResponse = await fetch(
-				'https://test-api.metaproprotocol.com/ms/users-service/auth/signature/hash',
-				{
-					headers: {
-						'x-account-wallet': account,
-					},
-				},
-			)
-			const {hash} = await hashResponse.json()
-
-			const balance = await provider.getBalance(account)
-
-			console.log(balance)
-
-			// Create a message to sign. Remember to include the account address and the hash. Account address should be in lowercase in every step
-			const verifyMessage = `Please sign to let us verify\nthat you are the owner of this address\n${account}\n\nRequest ID ${hash}`
-			const signature = await provider.send('personal_sign', [
-				verifyMessage,
+			const userResponse = await loginOrCreateAccount(
 				account,
-			])
-
-			// We can check if account is already registered in metapro
-			// const checkAccountRequest = await fetch(
-			// 	`https://test-api.metaproprotocol.com/ms/users-service/auth/check/wallet/${account}?projectId=${process.env.NEXT_PUBLIC_PROJECT_ID}`,
-			// )
-
-			// const checkWalletResponse = await checkAccountRequest.json()
-			// When user does not exists in metapro we can handle logic for example to check rules and create account
-			// if (!checkWalletResponse?.hasAccount) {
-			// 	// Handle logic for example check rules and create account
-			// 	return
-			// }
-
-			const createOrLoginAccountRequest = await fetch(
-				'https://test-api.metaproprotocol.com/ms/users-service/v2/auth/web3/login',
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'x-account-wallet': account,
-						'x-account-login-hash': hash,
-					},
-					body: JSON.stringify({
-						wallet: account,
-						signature,
-						rulesChecked: true, // This should be true if you have checked the rules
-						projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-					}),
-				},
+				rulesChecked,
+				provider,
 			)
-			const createAccountResponse = await createOrLoginAccountRequest.json()
-			setCurrentUserID(createAccountResponse?.account?.userId)
-		} catch (error) {
-			console.error(error)
-		}
-	}
-
-	const handleExampleTransaction = async () => {
-		try {
-			const provider = new Web3Provider(window.ethereum as ExternalProvider)
-			const transaction = await provider.send('personal_sign', [
-				'This is example transaction',
-				account,
-			])
-			console.log(transaction)
+			await push(`account/${userResponse?.account?.userId}`)
 		} catch (error) {
 			console.error(error)
 		}
 	}
 
 	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
-				<p>Wallet address: {account}</p>
-			</div>
-			<div className={styles.description}>
-				<p>Chain ID: {chainId}</p>
-			</div>
-			<div className={styles.description}>
-				<button
-					disabled={currentUserID.length > 0 || !provider}
-					onClick={handleMetaproLogin}
-				>
-					Login to metapro
-				</button>
-			</div>
-			<div className={styles.description}>
-				<button onClick={handleExampleTransaction}>
-					Send example transaction
-				</button>
-			</div>
-			<div className={styles.grid}></div>
-		</main>
+		<PageWrapper>
+			<UserDetailsTitle>Wallet address</UserDetailsTitle>
+			<UserDetails> {account}</UserDetails>
+			<UserDetailsTitle>Chain ID: {chainId}</UserDetailsTitle>
+			<UserDetails style={{marginBottom: '8px'}}>{chainId}</UserDetails>
+			<RegulationsWrapper>
+				{displayRulesCheckbox && (
+					<>
+						<input
+							type='checkbox'
+							onChange={e => {
+								setRulesChecked(e.target.checked)
+							}}
+							checked={rulesChecked}
+						/>
+						<span
+							style={{
+								marginLeft: '16px',
+								fontSize: '12px',
+								textTransform: 'uppercase',
+							}}
+						>
+							I have read the regulations and privacy policy of the website
+						</span>
+					</>
+				)}
+			</RegulationsWrapper>
+			<LoginButton
+				// disabled={!provider || !rulesChecked}
+				onClick={handleMetaproLogin}
+			/>
+			<TransactionButton
+				onClick={() => handleExampleSignature(account, provider)}
+			/>
+		</PageWrapper>
 	)
 }
