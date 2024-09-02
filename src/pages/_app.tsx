f5d99786a914782f725c20667df8f99e09259313
Jakub Antczak <jakubantczak.info@gmail.com>
2024-09-02 15:46:36 +0200

    MPRO-001: Created example UI for the demo app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type {ReactElement, ReactNode} from 'react'
 import type {NextPage} from 'next'
 import type {AppProps} from 'next/app'
-import './page.module.css'
 import './globals.css'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
