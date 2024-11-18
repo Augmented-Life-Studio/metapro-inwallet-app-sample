0a0dd11fe26fa292159d55b19a4d4d1ec13314f5
Jakub Antczak <jakubantczak.info@gmail.com>
2024-11-18 14:44:40 +0100

    MPRO-001: Removes provider dependency from useEffect
    Ensures useEffect cleanup runs only once by removing the provider dependency. This prevents potential issues with multiple event listener removals.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
 				provider.removeAllListeners('chainChanged')
 			}
 		}
-	}, [provider])
+	}, [])
 
 	const handleMetaproLogin = async (): Promise<
 		CreateOrLoginResponse | undefined
